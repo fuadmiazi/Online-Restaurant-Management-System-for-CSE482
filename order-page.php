b510558d55d0ff233830b30fa68e0478a8e32ebf
unknown <rafid.ahmmad.3@gmail.com>
2022-12-13 12:46:10 +0600

    slight changes

--- order-page.php
+++ order-page.php
@@ -124,11 +124,12 @@
                     id="search" autocomplete="off" placeholder="Search" />
 
             </div>
-            <button class="cart p-3 rounded-full bg-white relative">
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"
-                    width="30" height="30" viewBox="0 0 256 256" xml:space="preserve">
-                    <defs></defs>
-                    <g style="
+            <a href="shopping-cart.php">
+                <button class="cart p-3 rounded-full bg-white relative">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"
+                        width="30" height="30" viewBox="0 0 256 256" xml:space="preserve">
+                        <defs></defs>
+                        <g style="
                         stroke: none;
                         stroke-width: 0;
                         stroke-dasharray: none;
@@ -139,9 +140,9 @@
                         fill-rule: nonzero;
                         opacity: 1;
                      " transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)">
-                        <path
-                            d="M 73.713 65.44 H 27.689 c -3.566 0 -6.377 -2.578 -6.686 -6.13 c -0.21 -2.426 0.807 -4.605 2.592 -5.939 L 16.381 21.07 c -0.199 -0.889 0.017 -1.819 0.586 -2.53 s 1.431 -1.124 2.341 -1.124 H 87 c 0.972 0 1.884 0.471 2.446 1.263 c 0.563 0.792 0.706 1.808 0.386 2.725 l -7.798 22.344 c -1.091 3.13 -3.798 5.429 -7.063 5.999 l -47.389 8.281 c -0.011 0.001 -0.021 0.003 -0.032 0.005 c -0.228 0.04 -0.623 0.126 -0.568 0.759 c 0.056 0.648 0.48 0.648 0.708 0.648 h 46.024 c 1.657 0 3 1.343 3 3 S 75.37 65.44 73.713 65.44 z"
-                            style="
+                            <path
+                                d="M 73.713 65.44 H 27.689 c -3.566 0 -6.377 -2.578 -6.686 -6.13 c -0.21 -2.426 0.807 -4.605 2.592 -5.939 L 16.381 21.07 c -0.199 -0.889 0.017 -1.819 0.586 -2.53 s 1.431 -1.124 2.341 -1.124 H 87 c 0.972 0 1.884 0.471 2.446 1.263 c 0.563 0.792 0.706 1.808 0.386 2.725 l -7.798 22.344 c -1.091 3.13 -3.798 5.429 -7.063 5.999 l -47.389 8.281 c -0.011 0.001 -0.021 0.003 -0.032 0.005 c -0.228 0.04 -0.623 0.126 -0.568 0.759 c 0.056 0.648 0.48 0.648 0.708 0.648 h 46.024 c 1.657 0 3 1.343 3 3 S 75.37 65.44 73.713 65.44 z"
+                                style="
                            stroke: none;
                            stroke-width: 1;
                            stroke-dasharray: none;
@@ -152,7 +153,7 @@
                            fill-rule: nonzero;
                            opacity: 1;
                         " transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
-                        <circle cx="28.25" cy="75.8" r="6.5" style="
+                            <circle cx="28.25" cy="75.8" r="6.5" style="
                            stroke: none;
                            stroke-width: 1;
                            stroke-dasharray: none;
@@ -163,7 +164,7 @@
                            fill-rule: nonzero;
                            opacity: 1;
                         " transform="  matrix(1 0 0 1 0 0) " />
-                        <circle cx="68.28999999999999" cy="75.8" r="6.5" style="
+                            <circle cx="68.28999999999999" cy="75.8" r="6.5" style="
                            stroke: none;
                            stroke-width: 1;
                            stroke-dasharray: none;
@@ -174,9 +175,9 @@
                            fill-rule: nonzero;
                            opacity: 1;
                         " transform="  matrix(1 0 0 1 0 0) " />
-                        <path
-                            d="M 19.306 23.417 c -1.374 0 -2.613 -0.95 -2.925 -2.347 l -1.375 -6.155 c -0.554 -2.48 -2.716 -4.212 -5.258 -4.212 H 3 c -1.657 0 -3 -1.343 -3 -3 s 1.343 -3 3 -3 h 6.749 c 5.372 0 9.942 3.662 11.113 8.904 l 1.375 6.155 c 0.361 1.617 -0.657 3.221 -2.274 3.582 C 19.742 23.393 19.522 23.417 19.306 23.417 z"
-                            style="
+                            <path
+                                d="M 19.306 23.417 c -1.374 0 -2.613 -0.95 -2.925 -2.347 l -1.375 -6.155 c -0.554 -2.48 -2.716 -4.212 -5.258 -4.212 H 3 c -1.657 0 -3 -1.343 -3 -3 s 1.343 -3 3 -3 h 6.749 c 5.372 0 9.942 3.662 11.113 8.904 l 1.375 6.155 c 0.361 1.617 -0.657 3.221 -2.274 3.582 C 19.742 23.393 19.522 23.417 19.306 23.417 z"
+                                style="
                            stroke: none;
                            stroke-width: 1;
                            stroke-dasharray: none;
@@ -187,10 +188,11 @@
                            fill-rule: nonzero;
                            opacity: 1;
                         " transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
-                    </g>
-                </svg>
-                <div class="absolute -bottom-1 -right-1 bg-green-600 text-xs px-2 py-1 rounded-full">0</div>
-            </button>
+                        </g>
+                    </svg>
+                    <div class="absolute -bottom-1 -right-1 bg-green-600 text-xs px-2 py-1 rounded-full">0</div>
+                </button>
+            </a>
         </div>
 
         <div class="px-64 flex justify-between items-center mt-12 mb-12">
@@ -255,30 +257,30 @@
             </div>
         </div>
 
+        <form action="" method="post">
 
+            <div class="bg-[#282421] pt-12 z-50 relative  mx-64">
+                <p class="foodItems-title text-xl font-medium mb-14">Food Item List</p>
 
-        <div class="bg-[#282421] pt-12 z-50 relative  mx-64">
-            <p class="foodItems-title text-xl font-medium mb-14">Food Item List</p>
+                <div class="foodItems grid grid-cols-4 whitespace-nowrap pb-44 w-full gap-8" id="everyFoodItems">
 
-            <div class="foodItems grid grid-cols-4 whitespace-nowrap pb-44 w-full gap-8" id="everyFoodItems">
+                    <?php
 
-                <?php
+                    include("config.php");
 
-                include("config.php");
+                    $sql = "SELECT * FROM `food_items` ORDER BY `added_time` DESC";
+                    $result = $conn->query($sql);
 
-                $sql = "SELECT * FROM `food_items` ORDER BY `added_time` DESC";
-                $result = $conn->query($sql);
+                    if ($result->num_rows > 0) {
+                        // output data of each row
+                        while ($row = $result->fetch_assoc()) {
 
-                if ($result->num_rows > 0) {
-                    // output data of each row
-                    while ($row = $result->fetch_assoc()) {
-
-                        echo '<div
+                            echo '<div
             class="' . $row["type"] . ' rounded overflow-hidden bg-[#322d29] h-[400px] hover:scale-[1.02] transition-all duration-200">
             <a class="" href=""><img class="w-full h-[200px] object-cover" src="images/' . $row["image_name"] . '" alt=""
                     style="object-position: 20% 75%" />
                 <div class="p-5">
-                    <p class="font-medium text-center truncate">' . $row["name"] . '</p>
+                    <p class="food-item-name font-medium text-center truncate">' . $row["name"] . '</p>
                     <hr class="mt-5" />
                     <div class="flex text-sm justify-between items-center mt-5 font-bold">
                         <p>Price: ' . $row["price"] . '$</p>
@@ -291,21 +293,24 @@
             </a>
         </div>';
 
+                        }
+
+
+                    } else {
+                        echo "0 results";
                     }
 
-                } else {
-                    echo "0 results";
-                }
+                    $conn->close();
 
-                $conn->close();
+                    ?>
 
-                ?>
+        </form>
 
 
-            </div>
-        </div>
+    </div>
+    </div>
 
-        <?php require "footer.php" ?>
+    <?php require "footer.php" ?>
     </div>
     <script type="text/javascript">
 
@@ -378,8 +383,8 @@
                     let search_div = $("#search-result-div");
                     search_div.css("animation",
                         "slide-out 1s ease-in-out").one('animationend', () => {
-                        search_div.css("display", "none");
-                    });
+                            search_div.css("display", "none");
+                        });
                 }
             })
         })
